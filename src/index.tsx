bb40b1f3e6a7aa44b2c922e1a452533af3e8550e
kimtanook <seob6402@gmail.com>
2023-03-21 23:48:02 +0900

    Update root, provider code Add Recoil Root and Query Provider Code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const queryClient = new QueryClient();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </QueryClientProvider>
 );
