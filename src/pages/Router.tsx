fd48cb800120fea0d4cf4a36b29dad3958b7d77c
kimtanook <seob6402@gmail.com>
2023-03-21 23:54:29 +0900

    Update Add another route Add another route code

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Calendar from "./Calendar";
-import Diary from "./Diary";
+import DetailPost from "./diary/DetailPost";
+import Diary from "./diary/Diary";
+import PostForm from "./diary/PostForm";
+import Landing from "./Landing";
 import Main from "./Main";
 import Todo from "./Todo";
 
@@ -8,10 +11,13 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/diary" element={<Diary />} />
         <Route path="/todo" element={<Todo />} />
-        <Route path="/Calendar" element={<Calendar />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/post-form" element={<PostForm />} />
+        <Route path="/detail-diary/:postId" element={<DetailPost />} />
       </Routes>
     </BrowserRouter>
   );
