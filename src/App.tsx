29af38e36a3cdd9ffa5791a3ba41de4cb13ad325
kimtanook <seob6402@gmail.com>
2023-03-22 20:58:15 +0900

    Update App.tsx edit App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AppRouter from "./pages/Router";
+import { authService } from "./util/firebase";
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        navigate("/");
+      } else {
+        navigate("/landing");
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return <div>{init ? <AppRouter /> : <div>로딩 중...</div>}</div>;
 }
 
 export default App;
