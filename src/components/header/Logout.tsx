7f19a9acfee7fe010f70d9ab84709a998f3a5b41
kimtanook <seob6402@gmail.com>
2023-03-22 20:55:45 +0900

    Feat Logout component Add Logout component

---
+++ src/components/header/Logout.tsx
@@ -0,0 +1,22 @@
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../../util/firebase";
+
+function Logout() {
+  const navigate = useNavigate();
+  const onClickLogout = () => {
+    signOut(authService).then(() => {
+      alert("로그아웃 성공!");
+      navigate("/landing");
+    });
+  };
+  return (
+    <div>
+      {authService.currentUser ? (
+        <button onClick={onClickLogout}>로그아웃</button>
+      ) : null}
+    </div>
+  );
+}
+
+export default Logout;
