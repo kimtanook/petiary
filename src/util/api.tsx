bb2cf4ec7337bc67c0fb9a20ac4c09f28546b4a1
kimtanook <seob6402@gmail.com>
2023-03-22 21:04:06 +0900

    Feat delete pictures Delete pictures when leaving

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -5,12 +5,15 @@ import {
   orderBy,
   query,
 } from "firebase/firestore";
-import { dbService } from "./firebase";
+import { deleteObject, ref } from "firebase/storage";
+import { dbService, storageService } from "./firebase";
 
+// 다이어리 포스트 저장
 export const addDiaryPost = (data: any) => {
   addDoc(collection(dbService, "diary"), data);
 };
 
+// 다이어리 포스트 가져오기
 export const getDiaryPost = async () => {
   const response: any = [];
 
@@ -22,4 +25,15 @@ export const getDiaryPost = async () => {
   });
 
   return response;
+};
+
+export const deleteDiaryPostImage = ({ imageUrl }: any) => {
+  const desertRef = ref(storageService, `images/${imageUrl}`);
+
+  // 작성 중 이탈 시 해당 사진을 스토리지에서 삭제
+  deleteObject(desertRef)
+    .then(() => {})
+    .catch((error) => {
+      console.log("error : ", error);
+    });
 };
