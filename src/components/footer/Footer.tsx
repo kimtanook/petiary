2051062e835f3f1a78faa26ab436d045b74e64e8
kimtanook <seob6402@gmail.com>
2023-04-08 00:55:48 +0900

    Feat create footer create footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,48 @@
+import styled from "styled-components";
+import logo from "../../img/logo/logo-row2.png";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Wrap>
+      <FooterTop>
+        <Img src={logo} />
+        <FooterItem>서비스소개</FooterItem>
+        <FooterItem>이용약관</FooterItem>
+        <FooterItem>고객센터</FooterItem>
+        <FooterItem>개인정보 처리방침</FooterItem>
+      </FooterTop>
+      <FooterBottom>
+        <FooterItem>펫티어리 Petiary - Pet Diary</FooterItem>
+      </FooterBottom>
+    </Wrap>
+  );
 }
 
 export default Footer;
+
+const Wrap = styled.div`
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const FooterTop = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const Img = styled.img`
+  height: 32px;
+  margin-right: 8px;
+`;
+const FooterItem = styled.div`
+  margin: 4px;
+`;
+const FooterBottom = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
