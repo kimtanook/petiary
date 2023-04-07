1ffd6197631a1a23ac355bb71007de3755c4cb97
kimtanook <seob6402@gmail.com>
2023-04-08 00:59:49 +0900

    Update post list route edit post list route

--- src/components/diary/PostList.tsx
+++ src/components/diary/PostList.tsx
@@ -2,15 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { getDiaryPost } from "../../util/api";
+import { authService } from "../../util/firebase";
 
 function PostList() {
-  const { data: diaryPostData } = useQuery(["diaryData"], getDiaryPost);
+  const uid = authService.currentUser?.uid;
+  const { data: diaryPostData } = useQuery(["diaryData", uid], getDiaryPost);
   return (
     <div>
       <div>PostList</div>
       {diaryPostData?.map((data: any) => (
         <Link
-          to={`/detail-diary/${data.id}`}
+          to={`/diary/${data.id}`}
           state={{ postData: data }}
           key={uuidv4()}
         >
