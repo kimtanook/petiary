61cde62c2f73178d05943a4d0f882c49cd127861
kimtanook <seob6402@gmail.com>
2023-04-08 01:01:32 +0900

    Feat custom alert create recoil custom alert

--- src/util/atom.tsx
+++ src/util/atom.tsx
@@ -5,3 +5,7 @@ export const mdPostState = atom({
   key: `mdPostdata${uuidv4()}`,
   default: "",
 });
+export const Alert = atom({
+  key: `customAlert${uuidv4()}`,
+  default: { toggle: false, message: "" },
+});
