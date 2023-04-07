946893aa72ded829c2af41ab5fdb9c3fc87c2200
kimtanook <seob6402@gmail.com>
2023-04-08 00:54:21 +0900

    Feat diary, auth api add diary crud, auth user

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -1,23 +1,31 @@
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   getDocs,
   orderBy,
   query,
+  setDoc,
+  updateDoc,
+  where,
 } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import { dbService, storageService } from "./firebase";
 
-// 다이어리 포스트 저장
-export const addDiaryPost = (data: any) => {
-  addDoc(collection(dbService, "diary"), data);
+// 구글 로그인, 회원가입 시 유저 데이터 저장
+export const addUserData = async (data: any) => {
+  setDoc(doc(dbService, `user/${data.uid}`), data);
 };
-
-// 다이어리 포스트 가져오기
-export const getDiaryPost = async () => {
+// 전체공개 다이어리 포스트 가져오기
+export const getOpenDiaryPost = async () => {
   const response: any = [];
 
-  const q = query(collection(dbService, "diary"), orderBy("createdAt", "desc"));
+  const q = query(
+    collection(dbService, "diary"),
+    where("open", "==", true),
+    orderBy("createdAt", "desc")
+  );
 
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
@@ -26,11 +34,40 @@ export const getDiaryPost = async () => {
 
   return response;
 };
+// 내 다이어리 포스트 가져오기
+export const getDiaryPost = async ({ queryKey }: any) => {
+  const [_, uid] = queryKey;
+  const response: any = [];
+
+  const q = query(
+    collection(dbService, "diary"),
+    where("uid", "==", uid),
+    orderBy("createdAt", "desc")
+  );
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    response.push({ id: doc.id, ...doc.data() });
+  });
+
+  return response;
+};
+// 다이어리 포스트 저장
+export const addDiaryPost = (data: any) => {
+  addDoc(collection(dbService, "diary"), data);
+};
+// 다이어리 포스트 수정
+export const updateDiaryPost = (data: any) => {
+  return updateDoc(doc(dbService, `diary/${data.id}`), data);
+};
+// 다이어리 포스트 삭제
+export const deleteDiaryPost = (data: any) => {
+  deleteDoc(doc(dbService, `diary/${data.id}`));
+};
 
 export const deleteDiaryPostImage = ({ imageUrl }: any) => {
   const desertRef = ref(storageService, `images/${imageUrl}`);
-
-  // 작성 중 이탈 시 해당 사진을 스토리지에서 삭제
+  // 스토리지에서 이미지 삭제
   deleteObject(desertRef)
     .then(() => {})
     .catch((error) => {
