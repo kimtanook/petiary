3ebc21222a0b06436da88c6731ad92e47ebe7862
kimtanook <seob6402@gmail.com>
2023-04-08 00:54:52 +0900

    Update diary route edit diary route code

--- src/pages/diary/Diary.tsx
+++ src/pages/diary/Diary.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import PostList from "../../components/diary/PostList";
 function Diary() {
   return (
-    <div>
+    <Wrap>
       <div>Diary</div>
-      <Link to={"/post-form"}>
+      <Link to={"/diary/write"}>
         <div>포스트 작성하기</div>
       </Link>
       <PostList />
-    </div>
+    </Wrap>
   );
 }
 
 export default Diary;
+const Wrap = styled.div`
+  background-color: aqua;
+`;
