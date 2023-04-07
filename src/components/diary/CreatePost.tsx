1f8d82d01640ce9a7377ecf63e70a7ba091b9f4c
kimtanook <seob6402@gmail.com>
2023-04-08 00:58:47 +0900

    Feat diary post update add diary post update function

--- src/components/diary/CreatePost.tsx
+++ src/components/diary/CreatePost.tsx
@@ -5,18 +5,20 @@ import * as commands from "@uiw/react-md-editor/lib/commands";
 import "@uiw/react-md-editor/markdown-editor.css";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
-import { addDiaryPost, deleteDiaryPostImage } from "../../util/api";
+import { addDiaryPost, updateDiaryPost } from "../../util/api";
 import { mdPostState } from "../../util/atom";
-import { storageService } from "../../util/firebase";
+import { authService, storageService } from "../../util/firebase";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [mdContent, setMdContent] = useRecoilState<string>(mdPostState);
   const [diaryTitle, setDiaryTitle] = useState("");
-  const navigate = useNavigate();
+  const [openValue, setOpenValue] = useState(false);
   const fileInput = useRef<HTMLInputElement>(null);
 
   const onChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
@@ -25,7 +27,7 @@ const CreatePost = () => {
 
   const handleImageChange = (e: any) => {
     let file = e.target.files;
-    console.log("file : ", file);
+
     if (file.length === 0) {
       return;
     } else {
@@ -55,117 +57,184 @@ const CreatePost = () => {
   };
 
   const submitPost = () => {
-    addDiaryPost({
-      createdAt: Date.now(),
-      title: diaryTitle,
-      content: mdContent,
-    });
-    alert("작성완료!");
-    setMdContent("");
-    navigate("/diary");
+    if (!location.state) {
+      addDiaryPost({
+        uid: authService.currentUser?.uid,
+        createdAt: Date.now(),
+        title: diaryTitle,
+        content: mdContent,
+        open: openValue,
+      });
+      alert("작성완료!");
+      setMdContent("");
+      navigate("/diary");
+    } else {
+      updateDiaryPost({
+        id: location.state.id,
+        title: diaryTitle,
+        content: mdContent,
+        open: openValue,
+      });
+      alert("수정완료!");
+      setMdContent("");
+      navigate("/diary");
+    }
   };
 
-  const storageImageDelete = () => {
-    const regex: RegExp = /(\w+-\w+-\w+-\w+-\w+)/;
-    const match: RegExpMatchArray | null = mdContent.match(regex);
-
-    if (match) {
-      const extractedString: string = match[1];
-      console.log(extractedString); // 출력 결과: fcfa3678-4b1e-49a7-ba4d-404b504e808d
-      deleteDiaryPostImage({ imageUrl: extractedString });
+  const onChangeOpenValue = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "공개") {
+      setOpenValue(true);
+    } else {
+      setOpenValue(false);
     }
   };
 
   useEffect(() => {
-    return storageImageDelete();
+    if (location.state) {
+      setDiaryTitle(location.state.title);
+      setMdContent(location.state.content);
+    }
   }, []);
 
   return (
-    <MdEditorBox>
-      <Link to={"/diary"}>
-        <div
-          onClick={() => {
-            setMdContent("");
-          }}
-        >
-          뒤로가기
-        </div>
-      </Link>
-      <input onChange={onChangeTitle} placeholder="제목을 입력해주세요." />
-      <MDEditor
-        data-color-mode="light"
-        value={mdContent}
-        height={700}
-        onChange={(value: any) => {
-          setMdContent(value);
+    <Wrap>
+      <BackButton
+        onClick={() => {
+          setMdContent("");
+          navigate("/diary");
         }}
-        commands={[
-          // Custom Toolbars
-          commands.group(
-            [
-              commands.title1,
-              commands.title2,
-              commands.title3,
-              commands.title4,
-              commands.title5,
-              commands.title6,
-            ],
-            {
-              name: "title",
-              groupName: "title",
-              buttonProps: { "aria-label": "Insert title" },
-            }
-          ),
-          commands.divider,
-          commands.group([], {
-            name: "update",
-            groupName: "update",
-            icon: (
-              <svg viewBox="0 0 1024 1024" width="12" height="12">
-                <path
-                  fill="currentColor"
-                  d="M716.8 921.6a51.2 51.2 0 1 1 0 102.4H307.2a51.2 51.2 0 1 1 0-102.4h409.6zM475.8016 382.1568a51.2 51.2 0 0 1 72.3968 0l144.8448 144.8448a51.2 51.2 0 0 1-72.448 72.3968L563.2 541.952V768a51.2 51.2 0 0 1-45.2096 50.8416L512 819.2a51.2 51.2 0 0 1-51.2-51.2v-226.048l-57.3952 57.4464a51.2 51.2 0 0 1-67.584 4.2496l-4.864-4.2496a51.2 51.2 0 0 1 0-72.3968zM512 0c138.6496 0 253.4912 102.144 277.1456 236.288l10.752 0.3072C924.928 242.688 1024 348.0576 1024 476.5696 1024 608.9728 918.8352 716.8 788.48 716.8a51.2 51.2 0 1 1 0-102.4l8.3968-0.256C866.2016 609.6384 921.6 550.0416 921.6 476.5696c0-76.4416-59.904-137.8816-133.12-137.8816h-97.28v-51.2C691.2 184.9856 610.6624 102.4 512 102.4S332.8 184.9856 332.8 287.488v51.2H235.52c-73.216 0-133.12 61.44-133.12 137.8816C102.4 552.96 162.304 614.4 235.52 614.4l5.9904 0.3584A51.2 51.2 0 0 1 235.52 716.8C105.1648 716.8 0 608.9728 0 476.5696c0-132.1984 104.8064-239.872 234.8544-240.2816C258.5088 102.144 373.3504 0 512 0z"
-                />
-              </svg>
-            ),
-            children: (handle: any) => {
-              return (
-                <div style={{ width: 120, padding: 10 }}>
-                  <div>이미지선택</div>
-                  <input
-                    type="file"
-                    ref={fileInput}
-                    accept="image/*"
-                    onChange={(event) => {
-                      handleImageChange(event);
-                      handle.close();
-                    }}
-                    alt="image"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => {
-                      handle.close();
-                    }}
-                  >
-                    Close
-                  </button>
-                </div>
-              );
-            },
-
-            buttonProps: { "aria-label": "Insert title" },
-          }),
-        ]}
+      >
+        〈
+      </BackButton>
+      <TitleInput
+        onChange={onChangeTitle}
+        defaultValue={diaryTitle}
+        placeholder="제목을 입력해주세요."
       />
-      <button onClick={submitPost}>작성완료</button>
-    </MdEditorBox>
+      <OpenValueBox onChange={onChangeOpenValue}>
+        <OpenValue>
+          <input type="radio" name="openValue" value="비공개" />
+          비공개 일기
+        </OpenValue>
+        <OpenValue>
+          <input type="radio" name="openValue" value="공개" />
+          공개 일기
+        </OpenValue>
+      </OpenValueBox>
+      <EditorBox>
+        <MDEditor
+          data-color-mode="light"
+          value={mdContent}
+          height={600}
+          onChange={(value: any) => {
+            setMdContent(value);
+          }}
+          commands={[
+            // Custom Toolbars
+            commands.group(
+              [
+                commands.title1,
+                commands.title2,
+                commands.title3,
+                commands.title4,
+                commands.title5,
+                commands.title6,
+              ],
+              {
+                name: "title",
+                groupName: "title",
+                buttonProps: { "aria-label": "Insert title" },
+              }
+            ),
+            commands.divider,
+            commands.group([], {
+              name: "update",
+              groupName: "update",
+              icon: (
+                <svg viewBox="0 0 1024 1024" width="12" height="12">
+                  <path
+                    fill="currentColor"
+                    d="M716.8 921.6a51.2 51.2 0 1 1 0 102.4H307.2a51.2 51.2 0 1 1 0-102.4h409.6zM475.8016 382.1568a51.2 51.2 0 0 1 72.3968 0l144.8448 144.8448a51.2 51.2 0 0 1-72.448 72.3968L563.2 541.952V768a51.2 51.2 0 0 1-45.2096 50.8416L512 819.2a51.2 51.2 0 0 1-51.2-51.2v-226.048l-57.3952 57.4464a51.2 51.2 0 0 1-67.584 4.2496l-4.864-4.2496a51.2 51.2 0 0 1 0-72.3968zM512 0c138.6496 0 253.4912 102.144 277.1456 236.288l10.752 0.3072C924.928 242.688 1024 348.0576 1024 476.5696 1024 608.9728 918.8352 716.8 788.48 716.8a51.2 51.2 0 1 1 0-102.4l8.3968-0.256C866.2016 609.6384 921.6 550.0416 921.6 476.5696c0-76.4416-59.904-137.8816-133.12-137.8816h-97.28v-51.2C691.2 184.9856 610.6624 102.4 512 102.4S332.8 184.9856 332.8 287.488v51.2H235.52c-73.216 0-133.12 61.44-133.12 137.8816C102.4 552.96 162.304 614.4 235.52 614.4l5.9904 0.3584A51.2 51.2 0 0 1 235.52 716.8C105.1648 716.8 0 608.9728 0 476.5696c0-132.1984 104.8064-239.872 234.8544-240.2816C258.5088 102.144 373.3504 0 512 0z"
+                  />
+                </svg>
+              ),
+              children: (handle: any) => {
+                return (
+                  <div style={{ width: 120, padding: 10 }}>
+                    <div>이미지선택</div>
+                    <input
+                      type="file"
+                      ref={fileInput}
+                      accept="image/*"
+                      onChange={(event) => {
+                        handleImageChange(event);
+                        handle.close();
+                      }}
+                      alt="image"
+                    />
+                    <button
+                      type="button"
+                      onClick={() => {
+                        handle.close();
+                      }}
+                    >
+                      Close
+                    </button>
+                  </div>
+                );
+              },
+
+              buttonProps: { "aria-label": "Insert title" },
+            }),
+          ]}
+        />
+      </EditorBox>
+      <SubmitButton onClick={submitPost}>
+        {location.state ? "수정완료" : "작성완료"}
+      </SubmitButton>
+    </Wrap>
   );
 };
 
 export default CreatePost;
 
-const MdEditorBox = styled.div`
-  width: 1000px;
-  height: 400px;
+const Wrap = styled.div`
+  width: 80vw;
+`;
+
+const BackButton = styled.div`
+  cursor: pointer;
+  width: 24px;
+  font-size: 24px;
+  text-align: center;
+  margin: 4px;
+`;
+const TitleInput = styled.input`
+  width: 400px;
+  height: 40px;
+  font-size: 24px;
+  border-radius: 12px;
+  border: none;
+  padding-left: 12px;
+  margin: 4px;
+`;
+const OpenValueBox = styled.div`
+  margin: 4px;
+`;
+const OpenValue = styled.label`
+  cursor: pointer;
+`;
+const EditorBox = styled.div`
+  margin: 4px;
+`;
+const SubmitButton = styled.button`
+  cursor: pointer;
+  background-color: #ff8b32;
+  color: white;
+  border: none;
+  width: 100px;
+  height: 32px;
+  border-radius: 12px;
+  font-size: 16px;
+  margin: 4px;
 `;
