ac5c0f8f588e3c8eaa5cb746f1941ce6de635b27
kimtanook <seob6402@gmail.com>
2023-04-08 00:52:43 +0900

    Feat header style edit header style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logo from "../../img/logo-row2.png";
+import Logo from "../../img/logo/logo-row2.png";
 import Logout from "./Logout";
 
 function Header() {
@@ -13,8 +13,13 @@ function Header() {
 
 export default Header;
 const Wrap = styled.div`
+  box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  padding: 0px 16px 0px 16px;
   height: 70px;
+  background-color: white;
+  position: relative;
 `;
