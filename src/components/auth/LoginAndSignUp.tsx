509c0f9e812e6e45f3b13fc857b542abb8b8221d
kimtanook <seob6402@gmail.com>
2023-04-08 00:57:33 +0900

    Feat sign up user data save user data to sign up

--- src/components/auth/LoginAndSignUp.tsx
+++ src/components/auth/LoginAndSignUp.tsx
@@ -1,17 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import logo from "../../img/logo/logo-column.png";
+import { addUserData } from "../../util/api";
+import { Alert } from "../../util/atom";
 import { authService } from "../../util/firebase";
-// import logo from "../images/logo.png";
+import SocialLogin from "./SocialLogin";
 
 const LoginAndSignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState<any>("");
+  const [alertModal, setAlertModal] = useRecoilState(Alert);
+  const { mutate: addUserInfo } = useMutation(addUserData);
   const navigate = useNavigate();
 
   const onChange = (event: any) => {
@@ -32,14 +40,20 @@ const LoginAndSignUp = () => {
       if (!newAccount === true) {
         await createUserWithEmailAndPassword(authService, email, password).then(
           () => {
-            alert("회원가입 완료!");
+            const userInfo = {
+              uid: authService.currentUser?.uid,
+              userName: authService.currentUser?.displayName,
+              userImg: "",
+            };
+            addUserInfo(userInfo);
+            alert("회원가입 성공!");
             navigate("/main");
           }
         );
       } else {
         await signInWithEmailAndPassword(authService, email, password).then(
           () => {
-            alert("로그인 완료!");
+            alert("로그인 성공!");
             navigate("/main");
           }
         );
@@ -49,30 +63,42 @@ const LoginAndSignUp = () => {
         error.message === "Firebase: Error (auth/wrong-password)." ||
         error.message === "Firebase: Error (auth/user-not-found)."
       ) {
-        setError(alert("아이디 또는 비밀번호를 확인해주세요."));
+        setAlertModal({
+          toggle: true,
+          message: "아이디 또는 비밀번호를 확인해주세요.",
+        });
       } else if (
         error.message === "Firebase: Error (auth/email-already-in-use)."
       ) {
-        setError(alert("이미  사용중인 아이디 입니다."));
+        setAlertModal({
+          toggle: true,
+          message: "이미  사용중인 아이디 입니다.",
+        });
       } else if (
         error.message ===
         "Firebase: Password should be at least 6 characters (auth/weak-password)."
       ) {
-        setError(alert("비밀번호가 여섯글자 이상인지 확인해주세요."));
+        setAlertModal({
+          toggle: true,
+          message: "비밀번호가 여섯글자 이상인지 확인해주세요.",
+        });
       } else if (error.message === "Firebase: Error (auth/invalid-email).") {
-        setError(alert("이메일 형식이 맞는지 확인해주세요."));
+        setAlertModal({
+          toggle: true,
+          message: "이메일 형식이 맞는지 확인해주세요.",
+        });
       }
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
-    <div className="auth-form-container">
-      <div className="auth-title">{/* <img src={logo} alt="logo" /> */}</div>
-      <form className="auth-form" onSubmit={onSubmit}>
-        <div>
-          <input
-            className="input-form"
+    <Wrap>
+      <LandingTitle>펫티어리로 반려동물의 추억을 남겨보세요!</LandingTitle>
+      <LogoImg src={logo} alt="logo" />
+      <form onSubmit={onSubmit}>
+        <InputWrap>
+          <Input
             name="email"
             type="text"
             placeholder="Email"
@@ -80,10 +106,7 @@ const LoginAndSignUp = () => {
             value={email}
             onChange={onChange}
           />
-        </div>
-        <div>
-          <input
-            className="input-form"
+          <Input
             name="password"
             type="password"
             placeholder="Password"
@@ -91,20 +114,77 @@ const LoginAndSignUp = () => {
             value={password}
             onChange={onChange}
           />
-        </div>
-        <input
-          className="submit-button"
-          type="submit"
-          value={!newAccount ? "Create Account" : "Log In"}
-        />
-
+          <SubmitButton type="submit">
+            {!newAccount ? "계정 만들기" : "들어가기"}
+          </SubmitButton>
+        </InputWrap>
         {error}
       </form>
-      <div className="toggle-button" onClick={toggleAccount}>
-        {!newAccount ? "Go to Login" : "Go to sign up"}
-      </div>
-    </div>
+      <SignToggle onClick={toggleAccount}>
+        {!newAccount ? "로그인하러 가기" : "회원가입하러 가기"}
+      </SignToggle>
+      <SocialLogin />
+    </Wrap>
   );
 };
 
 export default LoginAndSignUp;
+
+const Wrap = styled.div`
+  width: 300px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const LandingTitle = styled.div`
+  font-size: 16px;
+  margin-bottom: 40px;
+`;
+const LogoImg = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Input = styled.input`
+  width: 192px;
+  height: 32px;
+  margin: 4px;
+  border-radius: 16px;
+  border: 1px solid #ff9524;
+  padding-left: 8px;
+`;
+const SubmitButton = styled.button`
+  cursor: pointer;
+  width: 200px;
+  height: 28px;
+  border: none;
+  border-radius: 8px;
+  margin: 4px;
+  color: white;
+  background-color: #ffcb88;
+  transition: 0.5s;
+  :hover {
+    transition: 0.5s;
+    background-color: #c8873c;
+  }
+`;
+const SignToggle = styled.div`
+  cursor: pointer;
+  margin: 8px 4px 0 4px;
+  border-radius: 8px;
+  width: 140px;
+  height: 24px;
+  line-height: 24px;
+  text-align: center;
+  background-color: #b56d3d;
+  color: white;
+  font-size: 12px;
+`;
