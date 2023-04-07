f3f9fa3d831f82ef69af7713155e9346d86dc366
kimtanook <seob6402@gmail.com>
2023-04-08 00:53:31 +0900

    Feat router component add route, edit style

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
+import Layout from "../components/Layout";
 import LeftCategory from "../components/category/LeftCategory";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import { authService } from "../util/firebase";
 import Calendar from "./Calendar";
-import DetailPost from "./diary/DetailPost";
-import Diary from "./diary/Diary";
-import PostForm from "./diary/PostForm";
 import Landing from "./Landing";
 import Main from "./Main";
 import Todo from "./Todo";
+import DetailPost from "./diary/DetailPost";
+import Diary from "./diary/Diary";
+import PostForm from "./diary/PostForm";
 
 const AppRouter = () => {
   const user = authService.currentUser;
@@ -19,17 +20,22 @@ const AppRouter = () => {
       {user ? <Header /> : null}
       <Wrap>
         {user ? <LeftCategory /> : null}
-        <Routes>
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="/todo" element={<Todo />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/post-form" element={<PostForm />} />
-          <Route path="/detail-diary/:postId" element={<DetailPost />} />
-        </Routes>
+        <ContentWrap>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/diary" element={<Diary />} />
+            <Route path="/diary/:postId" element={<DetailPost />} />
+            <Route path="/diary/write" element={<PostForm />} />
+            <Route path="/diary/write/:postId" element={<PostForm />} />
+            <Route path="/todo" element={<Todo />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/main/:postId" element={<DetailPost />} />
+          </Routes>
+        </ContentWrap>
       </Wrap>
       {user ? <Footer /> : null}
+      <Layout />
     </>
   );
 };
@@ -37,5 +43,10 @@ const AppRouter = () => {
 export default AppRouter;
 const Wrap = styled.div`
   display: flex;
-  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+  background-color: beige;
+`;
+const ContentWrap = styled.div`
+  padding: 12px;
 `;
