bb16de56d8bc9fa413ce925352095247acea1040
kimtanook <seob6402@gmail.com>
2023-04-14 22:30:42 +0900

    Update  post list add to stale time, cache time

--- src/components/diary/PostList.tsx
+++ src/components/diary/PostList.tsx
@@ -1,26 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 import { getDiaryPost } from "../../util/api";
 import { authService } from "../../util/firebase";
 
 function PostList() {
   const uid = authService.currentUser?.uid;
-  const { data: diaryPostData } = useQuery(["diaryData", uid], getDiaryPost);
+  const { data: diaryPostData } = useQuery(["diaryData", uid], getDiaryPost, {
+    staleTime: 1000 * 60 * 10,
+    cacheTime: 1000 * 60 * 10,
+  });
   return (
     <div>
-      <div>PostList</div>
       {diaryPostData?.map((data: any) => (
-        <Link
-          to={`/diary/${data.id}`}
-          state={{ postData: data }}
-          key={uuidv4()}
-        >
-          <div>{data.title}</div>
-        </Link>
+        <PostItemWrap>
+          <Link
+            to={`/diary/${data.id}`}
+            state={{ postData: data }}
+            key={uuidv4()}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <div>{data.title}</div>
+          </Link>
+        </PostItemWrap>
       ))}
     </div>
   );
 }
 
 export default PostList;
+const PostItemWrap = styled.div`
+  background-color: aqua;
+`;
