d108a3f4d9961609476dcbc9df1f64df390f431e
kimtanook <seob6402@gmail.com>
2023-04-14 22:35:03 +0900

    Update diary edit code, style

--- src/pages/diary/Diary.tsx
+++ src/pages/diary/Diary.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import PostList from "../../components/diary/PostList";
+import WriteImg from "../../img/icon/write.png";
 function Diary() {
   return (
     <Wrap>
       <div>Diary</div>
       <Link to={"/diary/write"}>
-        <div>포스트 작성하기</div>
+        <Write src={WriteImg} />
       </Link>
       <PostList />
     </Wrap>
@@ -14,6 +15,8 @@ function Diary() {
 }
 
 export default Diary;
-const Wrap = styled.div`
-  background-color: aqua;
+const Wrap = styled.div``;
+const Write = styled.img`
+  width: 32px;
+  height: 32px;
 `;
