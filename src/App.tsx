24755f282a07cdc75178baf404e8657db4192793
kimtanook <seob6402@gmail.com>
2023-04-14 22:23:24 +0900

    Feat add theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import Loading from "./components/common/Loading";
 import AppRouter from "./pages/Router";
 import { authService } from "./util/firebase";
 
 function App() {
   const [init, setInit] = useState(false);
+  const user = authService.currentUser;
   const navigate = useNavigate();
+  const paddingLeftRight = 32;
+  const theme = {
+    padding: `0 ${paddingLeftRight}px 0 ${paddingLeftRight}px`,
+  };
   useEffect(() => {
     onAuthStateChanged(authService, (user) => {
       if (user) {
@@ -19,7 +26,17 @@ function App() {
     });
   }, []);
 
-  return <div>{init ? <AppRouter /> : <div>로딩 중...</div>}</div>;
+  return (
+    <div>
+      {init ? (
+        <ThemeProvider theme={theme}>
+          <AppRouter />
+        </ThemeProvider>
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
 }
 
 export default App;
