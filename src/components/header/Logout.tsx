b34222314680b086e1f727db1a84d6775f87edd0
kimtanook <seob6402@gmail.com>
2023-04-14 22:32:12 +0900

    Update logout add to custom modal

--- src/components/header/Logout.tsx
+++ src/components/header/Logout.tsx
@@ -1,22 +1,36 @@
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import exitIcon from "../../img/icon/exit.png";
+import { Alert } from "../../util/atom";
 import { authService } from "../../util/firebase";
 
 function Logout() {
   const navigate = useNavigate();
+  const [alertModal, setAlertModal] = useRecoilState(Alert);
   const onClickLogout = () => {
     signOut(authService).then(() => {
-      alert("로그아웃 성공!");
+      setAlertModal({
+        toggle: true,
+        message: "로그아웃 성공!",
+      });
       navigate("/");
     });
   };
   return (
     <div>
       {authService.currentUser ? (
-        <button onClick={onClickLogout}>로그아웃</button>
+        <LogoutImg src={exitIcon} onClick={onClickLogout} />
       ) : null}
     </div>
   );
 }
 
 export default Logout;
+
+const LogoutImg = styled.img`
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+`;
