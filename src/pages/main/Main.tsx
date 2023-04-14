eca7128cf7fa2c687896457fcc74440eb0c5ed55
kimtanook <seob6402@gmail.com>
2023-04-14 22:35:21 +0900

    Update main edit component code

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import DiaryOpenPostList from "../../components/main/DiaryOpenPostList";
+
+function Main() {
+  return (
+    <Wrap>
+      <DiaryOpenPostList />
+    </Wrap>
+  );
+}
+
+export default Main;
+const Wrap = styled.div``;
