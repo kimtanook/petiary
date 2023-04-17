18c6c506cb1ac432e252129494046d61fb6bd217
kimtanook <seob6402@gmail.com>
2023-04-17 21:40:02 +0900

    Feat user image of header edit a user image to an image in DB

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../../img/logo/logo-row2.png";
+import { getUserData } from "../../util/api";
 import { authService } from "../../util/firebase";
 import Logout from "./Logout";
 
 function Header() {
   const navigate = useNavigate();
-  const userImg = authService.currentUser?.photoURL;
+  const user = authService.currentUser;
+  const { data: userData } = useQuery(["getUserData", user?.uid], getUserData);
 
   return (
     <Wrap>
       <img src={Logo} alt="logo" height={50} />
       <HeaderRight>
-        <Profile src={`${userImg}`} onClick={() => navigate("/my-page")} />
+        <Profile src={userData?.userImg} onClick={() => navigate("/my-page")} />
         <Logout />
       </HeaderRight>
     </Wrap>
@@ -34,10 +37,12 @@ const Wrap = styled.div`
 const HeaderRight = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   width: 80px;
 `;
 const Profile = styled.img`
   cursor: pointer;
-  width: 24px;
-  height: 24px;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
 `;
