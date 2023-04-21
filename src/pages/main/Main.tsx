e98ca5cb795dfd87cb1c6d06a63a2f1cdcb1bd23
kimtanook <seob6402@gmail.com>
2023-04-22 00:49:15 +0900

    Update main component edit little codes

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import DiaryOpenPostList from "../../components/main/DiaryOpenPostList";
+import Slide from "../../components/main/Slide";
+import { leftCategoryValue } from "../../util/atom";
 
 function Main() {
+  const [selectCategory, setSelectCategory] = useRecoilState(leftCategoryValue);
+  useEffect(() => {
+    setSelectCategory("");
+  }, []);
   return (
     <Wrap>
+      <Slide />
       <DiaryOpenPostList />
     </Wrap>
   );
