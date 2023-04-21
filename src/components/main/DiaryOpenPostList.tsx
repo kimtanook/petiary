d31d5d5e3f15d0149ded583b92813e12551ba620
kimtanook <seob6402@gmail.com>
2023-04-22 00:48:44 +0900

    Update diary open post list component edit little codes

--- src/components/main/DiaryOpenPostList.tsx
+++ src/components/main/DiaryOpenPostList.tsx
@@ -2,8 +2,10 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
+import moreBtnImg from "../../img/icon/angle-down.png";
 import { getOpenDiaryPost } from "../../util/api";
-import DiaryOpenPost from "./DiaryOpenPost";
+import Loading from "../common/Loading";
+import DiaryOpenPost from "../diary/DiaryPostItem";
 
 function DiaryOpenPostList() {
   // 무한 스크롤
@@ -22,27 +24,39 @@ function DiaryOpenPostList() {
       cacheTime: 1000 * 60 * 10,
     }
   );
-
+  const postLastIndex = openPost?.pages[openPost?.pages.length - 1];
   return (
     <Wrap>
-      <GridWrap>
-        {openPost?.pages.map((page: any) =>
-          page.map((item: any) => (
-            <Link
-              to={`/main/${item.id}`}
-              state={{ postData: item }}
-              key={uuidv4()}
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <DiaryOpenPost item={item} />
-            </Link>
-          ))
-        )}
-      </GridWrap>
-      {openPostStatus === "success" ? (
-        <MoreBtn onClick={() => fetchNextPage()}>더보기</MoreBtn>
+      {openPostStatus === "loading" ? (
+        <Loading />
       ) : (
-        <div>로딩중입니다...</div>
+        <>
+          <GridWrap>
+            {openPost?.pages.map((page: any) =>
+              page.map((item: any) => (
+                <Link
+                  to={`/main/${item.id}`}
+                  state={{ postData: item }}
+                  key={uuidv4()}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <DiaryOpenPost item={item} />
+                </Link>
+              ))
+            )}
+          </GridWrap>
+          <MoreBtn
+            onClick={() => {
+              fetchNextPage();
+              if (postLastIndex?.length === 0) {
+                alert("불러올 게시물이 없습니다.");
+              }
+            }}
+          >
+            <div>더보기</div>
+            <MortBtnImg src={moreBtnImg} alt="mortBtn" />
+          </MoreBtn>
+        </>
       )}
     </Wrap>
   );
@@ -56,32 +70,31 @@ const Wrap = styled.div`
 const GridWrap = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  @media screen and (min-width: 450px) {
+  @media screen and (min-width: 300px) {
     grid-template-columns: repeat(1, 1fr);
   }
-  @media screen and (min-width: 750px) {
+  @media screen and (min-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media screen and (min-width: 1104px) {
+  @media screen and (min-width: 1304px) {
     grid-template-columns: repeat(3, 1fr);
   }
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: 1660px) {
     grid-template-columns: repeat(4, 1fr);
   }
 `;
 const MoreBtn = styled.div`
   cursor: pointer;
   width: 100px;
-  height: 32px;
+  height: 56px;
   line-height: 32px;
   border-radius: 12px;
-  background-color: white;
   text-align: center;
   margin: auto;
   margin-top: 24px;
   font-weight: 700;
-  :hover {
-    background-color: #fccd7d;
-    color: white;
-  }
+`;
+const MortBtnImg = styled.img`
+  width: 20px;
+  height: 20px;
 `;
