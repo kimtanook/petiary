58eb5003f3c1100330aa2b1077bf2eebe974757e
kimtanook <seob6402@gmail.com>
2023-04-22 00:50:05 +0900

    Feat slide component create slide component to main

---
+++ src/components/main/Slide.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function Slide() {
+  return <Wrap>Slide</Wrap>;
+}
+
+export default Slide;
+const Wrap = styled.div`
+  background-color: aqua;
+  height: 400px;
+  margin-bottom: 12px;
+`;
