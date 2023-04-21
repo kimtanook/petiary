1f5ca1a2a9cf42129600872c1bc04a0f62cc002a
kimtanook <seob6402@gmail.com>
2023-04-22 00:54:39 +0900

    Update my profile Create a component that add information

--- src/components/mypage/EditProfileModal.tsx
+++ src/components/mypage/EditProfileModal.tsx
@@ -1,27 +1,33 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { updateProfile } from "firebase/auth";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
-import { editProfileImage } from "../../util/api";
+import { editProfileImage, getUserData } from "../../util/api";
 import { editProfileValue } from "../../util/atom";
 import { authService, storageService } from "../../util/firebase";
 
 function EditProfileModal() {
   const queryClient = useQueryClient();
   const user = authService.currentUser;
-  const { mutate: getUser } = useMutation(editProfileImage, {
+  const { data: currentUserData } = useQuery(
+    ["getUserData", user?.uid],
+    getUserData
+  );
+  const { mutate: editUser } = useMutation(editProfileImage, {
     onSuccess: () => queryClient.invalidateQueries(["getUserData"]),
   });
   const [editProfileToggle, setEditProfileToggle] =
     useRecoilState(editProfileValue);
   const [fileImageValue, setFileImageValue] = useState(user?.photoURL || "");
-  const [editNicknameValue, setEditNicknameValue] = useState(
-    user?.displayName || ""
-  );
-
+  const [nicknameValue, setNicknameValue] = useState(user?.displayName || "");
+  const [yearBirthValue, setYearBirthValue] = useState("");
+  const [monthBirthValue, setMonthBirthValue] = useState("");
+  const [dayBirthValue, setDayBirthValue] = useState("");
+  const [typeValue, setTypeValue] = useState("");
+  const [genderValue, setGenderValue] = useState("");
   const handleImageChange = (e: any) => {
     let file = e.target.files;
     if (file.length === 0) {
@@ -45,20 +51,60 @@ function EditProfileModal() {
     }
   };
   const onChangeNickname = (event: any) => {
-    setEditNicknameValue(event.currentTarget.value);
+    setNicknameValue(event.currentTarget.value);
+  };
+  const onChangeYearBirth = (event: any) => {
+    setYearBirthValue(event.currentTarget.value);
+  };
+  const onChangeDayBirth = (event: any) => {
+    setDayBirthValue(event.currentTarget.value);
+  };
+  const onChangeMonthBirth = (event: any) => {
+    setMonthBirthValue(event.currentTarget.value);
+  };
+  const onChangeType = (event: any) => {
+    setTypeValue(event.currentTarget.value);
+  };
+  const onChangeGender = (event: any) => {
+    setGenderValue(event.currentTarget.value);
   };
   const submitProfile = () => {
-    getUser({
+    if (
+      !nicknameValue ||
+      yearBirthValue.length !== 4 ||
+      !monthBirthValue ||
+      !dayBirthValue ||
+      !typeValue ||
+      !genderValue
+    ) {
+      return alert("필수 기재 항목을 확인해주세요.");
+    }
+    editUser({
       uid: user?.uid,
       userImg: fileImageValue,
-      userName: editNicknameValue,
+      userName: nicknameValue,
+      userYearBirth: yearBirthValue,
+      userMonthBirth: monthBirthValue,
+      userDayBirth: dayBirthValue,
+      userType: typeValue,
+      userGender: genderValue,
     });
     updateProfile(user!, {
       ...user,
-      displayName: editNicknameValue,
+      displayName: nicknameValue,
       photoURL: fileImageValue,
     }).then(() => setEditProfileToggle(false));
   };
+
+  useEffect(() => {
+    if (currentUserData) {
+      setYearBirthValue(currentUserData?.userYearBirth);
+      setMonthBirthValue(currentUserData?.userMonthBirth);
+      setDayBirthValue(currentUserData?.userDayBirth);
+      setTypeValue(currentUserData?.userType);
+      setGenderValue(currentUserData?.userGender);
+    }
+  }, [currentUserData]);
   return (
     <Wrap>
       <EditWrap>
@@ -72,11 +118,52 @@ function EditProfileModal() {
           />
         </label>
         <div>
-          수정할 닉네임 :{" "}
+          닉네임 :
           <input
             onChange={onChangeNickname}
-            value={editNicknameValue}
+            value={nicknameValue}
             maxLength={6}
+          />
+        </div>
+        <div>
+          생일 :
+          <input
+            onChange={onChangeYearBirth}
+            value={yearBirthValue}
+            maxLength={4}
+            placeholder="ex) 2000"
+          />
+          년
+          <input
+            onChange={onChangeMonthBirth}
+            value={monthBirthValue}
+            maxLength={2}
+            placeholder="ex) 1"
+          />
+          월
+          <input
+            onChange={onChangeDayBirth}
+            value={dayBirthValue}
+            maxLength={2}
+            placeholder="ex) 28"
+          />
+          일
+        </div>
+        <div>
+          종 :
+          <input
+            onChange={onChangeType}
+            value={typeValue}
+            placeholder="ex) 말티즈"
+          />
+        </div>
+        <div>
+          성별 :
+          <input
+            onChange={onChangeGender}
+            value={genderValue}
+            maxLength={1}
+            placeholder="ex) 여"
           />
         </div>
         <div onClick={submitProfile}>수정완료</div>
@@ -98,7 +185,7 @@ const Wrap = styled.div`
 `;
 
 const EditWrap = styled.div`
-  width: 300px;
+  width: 420px;
   height: 400px;
   background-color: beige;
   position: absolute;
--- src/components/mypage/Profile.tsx
+++ src/components/mypage/Profile.tsx
@@ -1,17 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { getUserData } from "../../util/api";
 import { editProfileValue } from "../../util/atom";
 import { authService } from "../../util/firebase";
 
 function Profile() {
   const user = authService.currentUser;
+  const { data: userData } = useQuery(["getUserData", user?.uid], getUserData);
   const [editProfileToggle, setEditProfileToggle] =
     useRecoilState(editProfileValue);
 
   return (
     <Wrap>
       <UserImg src={`${user?.photoURL}`} alt="user-img" />
-      <UserName>{user?.displayName}</UserName>
+      <div>
+        <UserName>{user?.displayName}</UserName>
+        <div>{userData?.userType}</div>
+        <div>{userData?.userGender}</div>
+        <div>
+          {userData?.userYearBirth}년 {userData?.userMonthBirth}월{" "}
+          {userData?.userDayBirth}일
+        </div>
+      </div>
       <EditProfileBtn onClick={() => setEditProfileToggle(true)}>
         프로필 수정
       </EditProfileBtn>
@@ -22,15 +33,18 @@ function Profile() {
 export default Profile;
 const Wrap = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   height: 200px;
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 0 12px 0 12px;
 `;
 
 const UserImg = styled.img`
   background-color: white;
-  border-radius: 50%;
+  border-radius: 12px;
   width: 100px;
   height: 100px;
   margin: 4px;
