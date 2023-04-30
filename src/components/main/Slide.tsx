10af22f2d9298cb60d7c983b07f2a1388e541b78
kimtanook <seob6402@gmail.com>
2023-04-30 13:25:03 +0900

    Update main image edit style

--- src/components/main/Slide.tsx
+++ src/components/main/Slide.tsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import mainImage from "../../img/main/main-image.png";
 
 function Slide() {
-  return <Wrap>Slide</Wrap>;
+  return <Wrap />;
 }
 
 export default Slide;
 const Wrap = styled.div`
-  background-color: aqua;
-  height: 400px;
-  margin-bottom: 12px;
+  background-image: url(${mainImage});
+  background-position: center;
+  background-size: cover;
+  border-radius: 12px 12px 0 0;
+  min-width: 360px;
+  height: 200px;
+  margin: 8px 0 12px 0;
 `;
