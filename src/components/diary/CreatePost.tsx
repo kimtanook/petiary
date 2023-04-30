8734343aa157090ccf104ea7725ce27605893bc9
kimtanook <seob6402@gmail.com>
2023-04-30 13:22:07 +0900

    Update create post component edit logo, style

--- src/components/diary/CreatePost.tsx
+++ src/components/diary/CreatePost.tsx
@@ -10,6 +10,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
+import photoImg from "../../img/icon/picture.png";
 import {
   createDiaryPost,
   myPostLastVisibleReset,
@@ -137,8 +138,8 @@ const CreatePost = () => {
       <TitleInput
         onChange={onChangeTitle}
         defaultValue={diaryTitle}
-        placeholder="제목 (최대 16글자)"
-        maxLength={16}
+        placeholder="제목 (최대 30글자)"
+        maxLength={30}
       />
       <OpenValueBox onChange={onChangeOpenValue}>
         <OpenValue>
@@ -190,12 +191,11 @@ const CreatePost = () => {
               name: "update",
               groupName: "update",
               icon: (
-                <svg viewBox="0 0 1024 1024" width="12" height="12">
-                  <path
-                    fill="currentColor"
-                    d="M716.8 921.6a51.2 51.2 0 1 1 0 102.4H307.2a51.2 51.2 0 1 1 0-102.4h409.6zM475.8016 382.1568a51.2 51.2 0 0 1 72.3968 0l144.8448 144.8448a51.2 51.2 0 0 1-72.448 72.3968L563.2 541.952V768a51.2 51.2 0 0 1-45.2096 50.8416L512 819.2a51.2 51.2 0 0 1-51.2-51.2v-226.048l-57.3952 57.4464a51.2 51.2 0 0 1-67.584 4.2496l-4.864-4.2496a51.2 51.2 0 0 1 0-72.3968zM512 0c138.6496 0 253.4912 102.144 277.1456 236.288l10.752 0.3072C924.928 242.688 1024 348.0576 1024 476.5696 1024 608.9728 918.8352 716.8 788.48 716.8a51.2 51.2 0 1 1 0-102.4l8.3968-0.256C866.2016 609.6384 921.6 550.0416 921.6 476.5696c0-76.4416-59.904-137.8816-133.12-137.8816h-97.28v-51.2C691.2 184.9856 610.6624 102.4 512 102.4S332.8 184.9856 332.8 287.488v51.2H235.52c-73.216 0-133.12 61.44-133.12 137.8816C102.4 552.96 162.304 614.4 235.52 614.4l5.9904 0.3584A51.2 51.2 0 0 1 235.52 716.8C105.1648 716.8 0 608.9728 0 476.5696c0-132.1984 104.8064-239.872 234.8544-240.2816C258.5088 102.144 373.3504 0 512 0z"
-                  />
-                </svg>
+                <img
+                  src={photoImg}
+                  alt="photoImg"
+                  style={{ width: "12px", height: "12px" }}
+                />
               ),
               children: (handle: any) => {
                 return (
@@ -238,7 +238,8 @@ const CreatePost = () => {
 export default CreatePost;
 
 const Wrap = styled.div`
-  width: 70vw;
+  width: 60vw;
+  min-width: 350px;
 `;
 
 const BackButton = styled.div`
@@ -249,7 +250,8 @@ const BackButton = styled.div`
   margin: 4px;
 `;
 const TitleInput = styled.input`
-  width: 400px;
+  width: 36vw;
+  min-width: 300px;
   height: 40px;
   font-size: 24px;
   border-radius: 12px;
