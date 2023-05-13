fc7a6ff30d60e26e4095a1c5806ee2eaf5a757b9
kimtanook <seob6402@gmail.com>
2023-05-13 19:02:15 +0900

    update delete transform button delete transform button

--- src/pages/diary/Diary.tsx
+++ src/pages/diary/Diary.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import TransformListBtn from "../../components/common/TransformListBtn";
 import PostList from "../../components/diary/PostList";
 import { leftCategoryValue } from "../../util/atom";
 function Diary() {
@@ -11,7 +10,6 @@ function Diary() {
   }, []);
   return (
     <Wrap>
-      <TransformListBtn />
       <PostList />
     </Wrap>
   );
