bd934e963d1f21ab537c0c5015ecb5bac15f6b80
kimtanook <seob6402@gmail.com>
2023-05-15 21:06:36 +0900

    update style

--- src/components/shelter_animal/AnimalItem.tsx
+++ src/components/shelter_animal/AnimalItem.tsx
@@ -19,9 +19,9 @@ function AnimalItem({ item }: any) {
           <NameGender>
             <Name>{kindCd}</Name>
             {item.sexCd === "M" ? (
-              <MaleImg src={maleImg} />
+              <GenderImg src={maleImg} />
             ) : (
-              <FemaleImg src={femaleImg} />
+              <GenderImg src={femaleImg} />
             )}
           </NameGender>
           <FlexBox>
@@ -40,11 +40,17 @@ function AnimalItem({ item }: any) {
 
 export default AnimalItem;
 const Wrap = styled.div`
+  padding-top: 8px;
   margin: 12px;
-  width: 200px;
+  width: 220px;
   height: 230px;
   box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.1);
   cursor: pointer;
+  @media screen and (max-width: 500px) {
+    width: 160px;
+    height: 190px;
+    margin: 4px;
+  }
   :hover {
     scale: 1.01;
   }
@@ -53,8 +59,14 @@ const Img = styled.div<{ urlImg: string }>`
   background-image: url(${(props) => props.urlImg});
   background-position: center;
   background-size: cover;
+  margin: auto;
   width: 200px;
   height: 120px;
+  @media screen and (max-width: 500px) {
+    width: 140px;
+    height: 100px;
+    margin: auto;
+  }
 `;
 const Info = styled.div`
   width: 180px;
@@ -62,15 +74,27 @@ const Info = styled.div`
   margin: 8px auto;
   overflow-y: scroll;
   font-size: 12px;
+  @media screen and (max-width: 500px) {
+    width: 140px;
+    margin: auto;
+  }
 `;
 const NameGender = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  @media screen and (max-width: 500px) {
+    width: 140px;
+    font-size: 16px;
+    height: 24px;
+  }
 `;
 const Name = styled.div`
   font-size: 20px;
   margin: 4px;
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+  }
 `;
 const FlexBox = styled.div`
   display: flex;
@@ -79,14 +103,15 @@ const FlexBox = styled.div`
   justify-content: start;
   margin: 4px;
 `;
-const MaleImg = styled.img`
+const GenderImg = styled.img`
   width: 20px;
   height: 20px;
+  @media screen and (max-width: 500px) {
+    width: 12px;
+    height: 12px;
+  }
 `;
-const FemaleImg = styled.img`
-  width: 20px;
-  height: 20px;
-`;
+
 const InfoImg = styled.img`
   width: 16px;
   margin-right: 8px;
--- src/pages/shelter_animal/DetailAnimal.tsx
+++ src/pages/shelter_animal/DetailAnimal.tsx
@@ -1,24 +1,52 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
+import femaleImg from "../../img/icon/female.png";
+import maleImg from "../../img/icon/male.png";
 
 function DetailAnimal() {
   const location = useLocation();
   const data = location.state;
-  console.log("data : ", data);
   const kindCd = data.kindCd.match(/\](.*)$/)[1];
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <Wrap>
       <Img src={data.popfile} />
       <InfoBox>
-        <div>{kindCd}</div>
-        <div>{data.age}</div>
-        <div>{data.weight}</div>
-        <div>{data.happenPlace}</div>
-        <div>{data.careNm}</div>
-        <div>{data.sexCd}</div>
-        <div>{data.neuterYn}</div>
-        <div>{data.specialMark}</div>
+        <KindBox>{kindCd}</KindBox>
+        <AgeBox>
+          <Title>나이</Title>
+          {data.age}
+        </AgeBox>
+        <WeightBox>
+          <Title>무게</Title>
+          {data.weight}
+        </WeightBox>
+        <PlaceBox>
+          <Title>발견장소</Title>
+          {data.happenPlace}
+        </PlaceBox>
+        <ShelterBox>
+          <Title>보호소</Title> {data.careNm}
+        </ShelterBox>
+        <GenderBox>
+          <Title>성별</Title>{" "}
+          <GenderImg
+            src={data.sexCd === "M" ? maleImg : femaleImg}
+            alt="gender-img"
+          />
+        </GenderBox>
+        <NeuterBox>
+          <Title>중성화 </Title>
+          {data.neuterYn === "Y" ? "완료" : "미완료"}
+        </NeuterBox>
+        <SpecialMarkBox>
+          <Title>특이사항</Title> {data.specialMark}
+        </SpecialMarkBox>
       </InfoBox>
     </Wrap>
   );
@@ -31,10 +59,45 @@ const Wrap = styled.div`
   align-items: center;
 `;
 const Img = styled.img`
-  height: 400px;
+  width: 100vw;
+  max-width: 500px;
 `;
 const InfoBox = styled.div`
-  background-color: aqua;
-  width: 70vw;
-  max-width: 800px;
+  margin-top: 12px;
+  width: 80vw;
+  max-width: 500px;
+`;
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  color: gray;
+`;
+const GenderBox = styled.div`
+  margin-bottom: 12px;
+`;
+const GenderImg = styled.img`
+  width: 20px;
+`;
+const KindBox = styled.div`
+  margin-bottom: 12px;
+  font-size: 24px;
+  font-weight: 700;
+`;
+const AgeBox = styled.div`
+  margin-bottom: 12px;
+`;
+const WeightBox = styled.div`
+  margin-bottom: 12px;
+`;
+const PlaceBox = styled.div`
+  margin-bottom: 12px;
+`;
+const ShelterBox = styled.div`
+  margin-bottom: 12px;
+`;
+const NeuterBox = styled.div`
+  margin-bottom: 12px;
+`;
+const SpecialMarkBox = styled.div`
+  margin-bottom: 12px;
 `;
